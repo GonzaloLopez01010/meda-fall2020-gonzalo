8d2d3e3df8f0388b040a3148e70f146db42c8097
Gonzalo L <gonzalolopez.work@gmail.com>
2020-09-30 13:09:15 -0700

    Created first JavaScript file. Added two data types: strings and numbers.

---
+++ assignments/week_5/firstjavascript.js
@@ -0,0 +1,36 @@
+//Console.log Takes the text within the quotes and sends it to the Javascript console.
+console.log("Hello World");
+
+console.log("What a lovely day!");
+
+console.log("I will be going on a walk after class.");
+
+console.log('This string use single quotes so if I include something that has a single quote inside I have to uses the "doubles quote".')
+
+// This is a single line comment.
+
+/*
+
+multi-line
+comment
+
+*/
+
+// DATATYPES
+// String
+"This line is a string, since its inside the double quotes";
+"To use single quotes 'and ' in the string, use double quotes at the end "
+''
+
+//Number(Dont use quotes )
+200;
+20.0;
+.200000;
+
+//Boolean
+true;
+false;
+
+console.log(200);
+console.log(20.0);
+console.log(.200000);
