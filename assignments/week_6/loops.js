1192f8d05eb56531a3a582078f85cd4fb223d7b4
Gonzalo L <gonzalolopez.work@gmail.com>
2020-10-05 12:44:53 -0700

    Created two new .js files, and worked with while loop, do while loop and for loop.

---
+++ assignments/week_6/loops.js
@@ -0,0 +1,62 @@
+// while loop
+
+
+if (true) {
+
+}
+
+var counter = 0;
+
+while (counter <  10) {
+    console.log("Running");
+    counter = counter + 1;
+}
+
+// do while loop 
+
+while (false){
+
+}
+
+do {
+    // This line WILL AWLWAYS run atleast 1 time, becasue we execute the code first AND THEN we check the condition. 
+} while (false);
+
+// for loop
+
+for (var i = 0; i < 1000; i = i+ 1) {
+    console.log("Running");
+    console.log(i);
+}
+
+//Shortcuts
+var someNumber = 10;
+
+someNumber++; // someNumber = someNumber +1;
+someNumber--; // someNumber = someNumber -1;
+someNumber += 2; // someNumber = someNumber +2;
+someNumber -= 2; // someNumber = someNumber -2;
+someNumber /= 3;
+someNumber *= 3;
+
+//function sayRunning() {
+//    console.log("Running");
+// }
+
+// sayRunning 
+
+for (var i = 0; i < 10; i = i + 1 ) {
+    console.log("running with break keyword.");
+
+    if (i == 6) {
+        break;
+    }
+}
+
+for (var i = 0; i < 10; i = i + 1 ) {
+    if (true) {
+        continue;
+    }
+
+    console.log("running with continue keyword.");
+}
---
+++ assignments/week_6/review.js
@@ -0,0 +1,32 @@
+//Data Types
+var aString = "Today is very cold. I like it";
+
+var aNumber = 40;
+
+var aBoolean = true;
+
+//Variable 
+
+var thisIsAReallyLongVariableName;
+
+var b;
+
+var userAge = 39;
+var userAddress;
+
+//if statements
+
+if (userAge > 18){
+    //Do something if userAge is over 18
+}else{
+
+}
+
+//Functions
+function afunction(){
+    //Do something when we call aFunction()
+}
+
+aFunction();
+
+//
