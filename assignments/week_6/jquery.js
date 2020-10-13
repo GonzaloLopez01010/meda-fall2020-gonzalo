b60681fbc81d26b09f7f28c4504526f079934112
Gonzalo L <gonzalolopez.work@gmail.com>
2020-10-13 13:09:29 -0700

    Worked with jQuery, .click and .css.

--- assignments/week_6/jquery.js
+++ assignments/week_6/jquery.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
+$(function() {
     console.log("jquery is loaded now.");
 
     // This will make the background color of my jquery.html file blue.
     $("body").css("background-color","Blue");
 });
+
+// Another way to write the $() functionis is jQuery();
---
+++ assignments/week_7/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function (){
+    
+    setTimeout(function (){
+        var box = $("#thisBox"); 
+        box.css("background-color","red");
+    }, 3200);
+
+    setTimeout(function (){
+        var box = $("#thisBox"); 
+        box.css("background-color","blue");
+    }, 6200);
+
+    var greenButton = $("#green-button");
+
+    greenButton.click(function () {
+        $("#thisBox").css("background-color", "green");
+    });
+
+    var yellowButton = $("#yellow-button");
+
+    yellowButton.click(function () {
+        $("#thisBox").css("background-color", "yellow");
+    });
+
+});
